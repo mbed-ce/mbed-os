9e7fadafe77548adcdd34f3bef16525b92bee9cb
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2022-10-08 23:14:36 -0700

    First attempt at converting to OBJECT libraries.  Not working due to more object library limitations

--- tools/cmake/mbed_create_distro.cmake
+++ tools/cmake/mbed_create_distro.cmake
@@ -1,24 +1,8 @@
 # Copyright (c) 2021 ARM Limited. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-# This script provides mbed_create_distro(), a function that lets you compile multiple
-# apps that use Mbed OS without waiting for Mbed OS to build multiple times.
-
-# You can use it like this:
-# mbed_create_distro(mbed_for_my_app mbed-os mbed-storage-kvstore mbed-storage-filesystem)
-#
-# add_executable(myapp1 MyApp1.cpp)
-# target_link_libraries(myapp1 PRIVATE mbed_for_my_app)
-# mbed_set_post_build(myapp1)
-#
-# add_executable(myapp2 MyApp2.cpp)
-# target_link_libraries(myapp2 PRIVATE mbed_for_my_app)
-# mbed_set_post_build(myapp2)
-#
-# Both myapp1 and myapp2 will act like they were linked to mbed-os, mbed-storage-kvstore,
-# and mbed-storage-filesystem.  Note that if you actually did target_link_libraries(myapp1 PRIVATE mbed-os
-# mbed-storage-kvstore mbed-storage-filesystem), it would compile a new version of the Mbed OS source
-# files for each target. However, using mbed_create_distro(), Mbed OS will only be compiled once.
+# This script provides mbed_create_distro(), a function that combines a tree of INTERFACE targets
+# into a single library target, copying properties appropriately.
 
 # Append the value of PROPERTY from SOURCE to the value of PROPERTY on DESTINATION
 function(copy_append_property PROPERTY SOURCE DESTINATION)
@@ -32,7 +16,7 @@ endfunction(copy_append_property)
 # Create a "distribution" of Mbed OS containing the base Mbed and certain modules.
 # This distribution only needs to be compiled once and can be referenced in an arbitrary amount of targets.
 function(mbed_create_distro NAME) # ARGN: modules...
-	add_library(${NAME} STATIC EXCLUDE_FROM_ALL)
+	mbed_add_library(${NAME} EXCLUDE_FROM_ALL)
 
 	# Now copy include dirs, compile defs, and compile options (but NOT interface source files) over
 	# to the distribution target so they will be passed into things that link to it.
@@ -46,56 +30,63 @@ function(mbed_create_distro NAME) # ARGN: modules...
 		list(GET REMAINING_MODULES 0 CURR_MODULE)
 		list(REMOVE_AT REMAINING_MODULES 0)
 
-		get_property(CURR_MODULE_TYPE TARGET ${CURR_MODULE} PROPERTY TYPE)
+		if(TARGET "${CURR_MODULE}")
 
-		# Pass up mbed linker script property, which is used by the top level code to select the linker script
-		copy_append_property(INTERFACE_MBED_LINKER_SCRIPT ${CURR_MODULE} ${NAME})
+			get_property(CURR_MODULE_TYPE TARGET ${CURR_MODULE} PROPERTY TYPE)
 
-		if("${CURR_MODULE_TYPE}" STREQUAL "STATIC_LIBRARY")
-			# Don't need to do anything other than linking it
-			target_link_libraries(${NAME} PUBLIC ${CURR_MODULE})
-		else()
-			# Make sure that linking to the distro pulls in the compiled code from CURR_MODULE
-			target_link_libraries(${NAME} PRIVATE ${CURR_MODULE})
+			# Pass up mbed linker script property, which is used by the top level code to select the linker script
+			copy_append_property(INTERFACE_MBED_LINKER_SCRIPT ${CURR_MODULE} ${NAME})
 
-			copy_append_property(INTERFACE_COMPILE_DEFINITIONS ${CURR_MODULE} ${NAME})
-			copy_append_property(INTERFACE_COMPILE_OPTIONS ${CURR_MODULE} ${NAME})
-			copy_append_property(INTERFACE_INCLUDE_DIRECTORIES ${CURR_MODULE} ${NAME})
-			copy_append_property(INTERFACE_LINK_OPTIONS ${CURR_MODULE} ${NAME})
+			if("${CURR_MODULE_TYPE}" STREQUAL "STATIC_LIBRARY")
+				# Don't need to do anything other than linking it
+				target_link_libraries(${NAME} PUBLIC ${CURR_MODULE})
+			else()
+				# Make sure that linking to the distro pulls in the compiled code from CURR_MODULE
+				target_link_libraries(${NAME} PRIVATE ${CURR_MODULE})
 
-			# CMake currently has a limitation that OBJECT libraries cannot link to other OBJECT libraries
-			# via the LINK_LIBRARIES property -- CMake will not link the objects in properly :/.
-			# see: https://cmake.org/pipermail/cmake/2019-May/069453.html
-			# also: https://gitlab.kitware.com/cmake/cmake/-/issues/18090
+				copy_append_property(INTERFACE_COMPILE_DEFINITIONS ${CURR_MODULE} ${NAME})
+				copy_append_property(INTERFACE_COMPILE_OPTIONS ${CURR_MODULE} ${NAME})
+				copy_append_property(INTERFACE_INCLUDE_DIRECTORIES ${CURR_MODULE} ${NAME})
+				copy_append_property(INTERFACE_LINK_OPTIONS ${CURR_MODULE} ${NAME})
 
-			if("${CURR_MODULE_TYPE}" STREQUAL "OBJECT_LIBRARY")
-				target_sources(${NAME} INTERFACE $<TARGET_OBJECTS:${CURR_MODULE}>)
+				# CMake currently has a limitation that OBJECT libraries cannot link to other OBJECT libraries
+				# via the LINK_LIBRARIES property -- CMake will not link the objects in properly :/.
+				# see: https://cmake.org/pipermail/cmake/2019-May/069453.html
+				# also: https://gitlab.kitware.com/cmake/cmake/-/issues/18090
 
-				# Check if this object library has any other libraries exported through its INTERFACE_SOURCES.
-				# If it does, we need to propagate those too.
-				get_property(OBJ_INTERFACE_SOURCES TARGET ${NAME} PROPERTY INTERFACE_SOURCES)
-				foreach(INTERFACE_SOURCE ${OBJ_INTERFACE_SOURCES})
-					if(INTERFACE_SOURCE MATCHES "\\$<TARGET_OBJECTS:.*>")
-						target_sources(${NAME} INTERFACE ${INTERFACE_SOURCE})
+				if("${CURR_MODULE_TYPE}" STREQUAL "OBJECT_LIBRARY")
+					target_sources(${NAME} INTERFACE $<TARGET_OBJECTS:${CURR_MODULE}>)
+
+					# Check if this object library has any other libraries exported through its INTERFACE_SOURCES.
+					# If it does, we need to propagate those too.
+					get_property(OBJ_INTERFACE_SOURCES TARGET ${NAME} PROPERTY INTERFACE_SOURCES)
+					foreach(INTERFACE_SOURCE ${OBJ_INTERFACE_SOURCES})
+						if(INTERFACE_SOURCE MATCHES "\\$<TARGET_OBJECTS:.*>")
+							target_sources(${NAME} INTERFACE ${INTERFACE_SOURCE})
+						endif()
+					endforeach()
+				endif()
+
+				list(APPEND COMPLETED_MODULES ${CURR_MODULE})
+
+				# find sub-modules of this module
+				get_property(SUBMODULES TARGET ${CURR_MODULE} PROPERTY INTERFACE_LINK_LIBRARIES)
+				foreach(SUBMODULE ${SUBMODULES})
+					if(NOT "${SUBMODULE}" MATCHES "::@") # remove CMake internal CMAKE_DIRECTORY_ID_SEP markers
+						# Remove LINK_ONLY genexes from target_link_libraries(... PRIVATE).  We can ignore things wrapped in these
+						# because they will already have been handled by the target_link_libraries earlier on.
+						if(NOT "${SUBMODULE}" MATCHES "\\$<LINK_ONLY:.*>")
+							if(NOT ${SUBMODULE} IN_LIST COMPLETED_MODULES)
+								list(APPEND REMAINING_MODULES ${SUBMODULE})
+							endif()
+						endif()
 					endif()
 				endforeach()
 			endif()
-
-			list(APPEND COMPLETED_MODULES ${CURR_MODULE})
-
-			# find sub-modules of this module
-			get_property(SUBMODULES TARGET ${CURR_MODULE} PROPERTY INTERFACE_LINK_LIBRARIES)
-			foreach(SUBMODULE ${SUBMODULES})
-				if(NOT "${SUBMODULE}" MATCHES "::@") # remove CMake internal CMAKE_DIRECTORY_ID_SEP markers
-					# Remove LINK_ONLY genexes from target_link_libraries(... PRIVATE).  We can ignore things wrapped in these
-					# because they will already have been handled by the target_link_libraries earlier on.
-					if(NOT "${SUBMODULE}" MATCHES "\\$<LINK_ONLY:.*>")
-						if(NOT ${SUBMODULE} IN_LIST COMPLETED_MODULES)
-							list(APPEND REMAINING_MODULES ${SUBMODULE})
-						endif()
-					endif()
-				endif()
-			endforeach()
+		else()
+			# Module is not a target, it's something else like a linker flag or a generator expression
+			# So, just link it to the output.
+			target_link_libraries(${NAME} INTERFACE ${CURR_MODULE})
 		endif()
 
 	endwhile()
