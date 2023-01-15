cb986656c1fed61ae569624d8e4fc246fdb26cc1
Johnk1987 <odiin@seznam.cz>
2023-01-15 21:00:21 +0100

    correction of STM32F411xE system_clock.c

--- targets/TARGET_STM/TARGET_STM32F4/TARGET_STM32F411xE/system_clock.c
+++ targets/TARGET_STM/TARGET_STM32F4/TARGET_STM32F411xE/system_clock.c
@@ -150,7 +150,7 @@ MBED_WEAK uint8_t SetSysClock_PLL_HSE(uint8_t bypass)
 /******************************************************************************/
 /*            PLL (clocked by HSI) used as System clock source                */
 /******************************************************************************/
-uint8_t SetSysClock_PLL_HSI(void)
+MBED_WEAK uint8_t SetSysClock_PLL_HSI(void)
 {
     RCC_OscInitTypeDef RCC_OscInitStruct;
     RCC_ClkInitTypeDef RCC_ClkInitStruct;
