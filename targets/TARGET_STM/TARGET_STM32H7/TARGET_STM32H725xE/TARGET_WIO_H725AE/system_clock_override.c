9f675d2fe873cb7e1f685b713d2619dc6654ba2a
JohnK1987 <Odiin@seznam.cz>
2023-02-18 11:16:32 +0100

    Licence filled from original source file

--- targets/TARGET_STM/TARGET_STM32H7/TARGET_STM32H725xE/TARGET_WIO_H725AE/system_clock_override.c
+++ targets/TARGET_STM/TARGET_STM32H7/TARGET_STM32H725xE/TARGET_WIO_H725AE/system_clock_override.c
@@ -1,10 +1,17 @@
-/**
-  ******************************************************************************
-  * @attention
-  * <h2><center>&copy; Copyright (c) 2023 Mbed-CE
-  *
-  ******************************************************************************
-**/
+/* mbed Microcontroller Library
+ * SPDX-License-Identifier: BSD-3-Clause
+ ******************************************************************************
+ *
+ * Copyright (c) 2015-2021 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
 
 /**
   * This file configures the system clock as follows:
