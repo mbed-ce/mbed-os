07856cdae0e470b3195de160fa4bc468455815fa
Johnk1987 <odiin@seznam.cz>
2023-02-18 23:40:56 +0100

    Correction of upload methods for WIO_H725AE

--- targets/upload_method_cfg/WIO_H725AE.cmake
+++ targets/upload_method_cfg/WIO_H725AE.cmake
@@ -5,9 +5,8 @@
 # Notes:
 # 1. Keep in mind the WIO_H725AE does not contain any on-board debugger, so all upload methods counts with external debugger, usually ST-Link.
 # 2. Using the JLINK upload method with your dev board requires converting its ST-LINK (versions lower than 3) into a J-Link.  See here for details: https://www.segger.com/products/debug-probes/j-link/models/other-j-links/st-link-on-board/
-# 3. PyOCD, OpenOCD and and ST-LINK (open source) are not working for some reason and for J-link a I was not able to made the test, because I am using ST-Link V3.
-# 4. If your target is not natively supported by the pyOCD, then you need install a keil package for family of your target by hands. Type "pyocd pack show" to console and you will see a list of already installed packages.
-# - If any package for your family is not on the list, then you need install them via command "pyocd pack install stm32h7" (take long time).Then just type "pyocd pack find STM32h7" or "pyocd pack find STM32h725" and you will see the part name of your target.
+# 3. PyOCD reports error "No cores were discovered!" 
+# 4. OpenOCD tested with ST-Link V2-1
 
 # General config parameters
 # -------------------------------------------------------------
@@ -22,7 +21,7 @@ set(MBED_RESET_BAUDRATE 115200)
 # Config options for JLINK
 # -------------------------------------------------------------
 
-set(JLINK_UPLOAD_ENABLED FALSE)
+set(JLINK_UPLOAD_ENABLED TRUE)
 set(JLINK_CPU_NAME STM32H725AE)
 set(JLINK_CLOCK_SPEED 4000)
 set(JLINK_UPLOAD_INTERFACE SWD)
@@ -37,9 +36,9 @@ set(PYOCD_CLOCK_SPEED 4000k)
 # Config options for OPENOCD
 # -------------------------------------------------------------
 
-set(OPENOCD_UPLOAD_ENABLED FLASE)
+set(OPENOCD_UPLOAD_ENABLED TRUE)
 set(OPENOCD_CHIP_CONFIG_COMMANDS
-    -f ${OpenOCD_SCRIPT_DIR}/board/st_x.cfg)
+    -f ${CMAKE_CURRENT_LIST_DIR}/openocd_cfgs/wio_h725.cfg)
 
 # Config options for STM32Cube
 # -------------------------------------------------------------
@@ -51,6 +50,6 @@ set(STM32CUBE_GDBSERVER_ARGS --swd)
 # Config options for stlink
 # -------------------------------------------------------------
 
-set(STLINK_UPLOAD_ENABLED FALSE)
+set(STLINK_UPLOAD_ENABLED TRUE)
 set(STLINK_LOAD_ADDRESS 0x8000000)
 set(STLINK_ARGS --connect-under-reset)
