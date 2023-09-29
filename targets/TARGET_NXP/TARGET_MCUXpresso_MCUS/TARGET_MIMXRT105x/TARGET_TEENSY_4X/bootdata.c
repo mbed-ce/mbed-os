f96dcaf2576aaf5c7ba1cbe2ecd745e70b172d6c
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2023-09-29 00:25:08 -0700

    Refactor flash setup for MIMXRT105x devices, adds flash support for MIMXRT1062 EVK

--- targets/TARGET_NXP/TARGET_MCUXpresso_MCUS/TARGET_MIMXRT105x/TARGET_TEENSY_4X/bootdata.c
+++ targets/TARGET_NXP/TARGET_MCUXpresso_MCUS/TARGET_MIMXRT105x/TARGET_TEENSY_4X/bootdata.c
@@ -6,6 +6,7 @@
 // - #define names had to be modified to match Mbed
 // - _flashimagelen replaced with __USED_FLASH_SIZE
 // - ResetHandler replaced with Reset_Handler
+// - hardcoded flash size replaced with BOARD_FLASH_SIZE
 
 /* Teensyduino Core Library
  * http://www.pjrc.com/teensy/
@@ -40,6 +41,7 @@
  */
 
 #include <stdint.h>
+#include <mimxrt_flash_info.h>
 
 extern void Reset_Handler(void);
 extern unsigned long _estack;
@@ -105,7 +107,7 @@ uint32_t FlexSPI_NOR_Config[128] = {
 #elif defined(TARGET_TEENSY_41)
 	0x00800000,		// sflashA1Size			0x50
 #else
-#error "Unknow flash chip size";
+#error "Unknown flash chip size";
 #endif
 	0,			// sflashA2Size
 	0,			// sflashB1Size
