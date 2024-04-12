a5b8cb31c72c7b41928f17bc56834a80bfed4437
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2024-04-11 22:11:34 -0700

    Start on Arduino Giga

---
+++ targets/upload_method_cfg/ARDUINO_GIGA_CM7.cmake
@@ -0,0 +1,53 @@
+# Mbed OS upload method configuration file for target ARDUINO_GIGA_CM7.
+# To change any of these parameters from their default values, set them in your build script between where you
+# include app.cmake and where you add mbed os as a subdirectory.
+
+# Notes:
+# 1. To use this target with PyOCD, you need to install a pack: `pyocd pack install STM32H747XIHx`.
+#    You might also need to run `pyocd pack update` first.
+
+# General config parameters
+# -------------------------------------------------------------
+set(UPLOAD_METHOD_DEFAULT MBED)
+
+# Config options for MBED
+# -------------------------------------------------------------
+
+set(MBED_UPLOAD_ENABLED TRUE)
+set(MBED_RESET_BAUDRATE 115200)
+
+# Config options for JLINK
+# -------------------------------------------------------------
+
+set(JLINK_UPLOAD_ENABLED FALSE)
+set(JLINK_CPU_NAME STM32H743ZI)
+set(JLINK_CLOCK_SPEED 4000)
+set(JLINK_UPLOAD_INTERFACE SWD)
+
+# Config options for PYOCD
+# -------------------------------------------------------------
+
+set(PYOCD_UPLOAD_ENABLED TRUE)
+set(PYOCD_TARGET_NAME STM32H747XIHx)
+set(PYOCD_CLOCK_SPEED 4000k)
+
+# Config options for OPENOCD
+# -------------------------------------------------------------
+
+set(OPENOCD_UPLOAD_ENABLED TRUE)
+set(OPENOCD_CHIP_CONFIG_COMMANDS
+    -f ${CMAKE_CURRENT_LIST_DIR}/openocd_cfgs/stm32h747.cfg)
+
+# Config options for STM32Cube
+# -------------------------------------------------------------
+
+set(STM32CUBE_UPLOAD_ENABLED TRUE)
+set(STM32CUBE_CONNECT_COMMAND -c port=SWD reset=HWrst)
+set(STM32CUBE_GDBSERVER_ARGS --swd --initialize-reset --apid 0)
+
+# Config options for stlink
+# -------------------------------------------------------------
+
+set(STLINK_UPLOAD_ENABLED TRUE)
+set(STLINK_LOAD_ADDRESS 0x8000000)
+set(STLINK_ARGS --connect-under-reset)
