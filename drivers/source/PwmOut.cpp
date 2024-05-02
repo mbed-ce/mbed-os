337923e8fe8ca3a685a0a47ed3dfa36723087fb0
Jamie Smith <smit109@usc.edu>
2024-05-02 09:49:45 -0700

    Fix a few more issues

--- drivers/source/PwmOut.cpp
+++ drivers/source/PwmOut.cpp
@@ -144,7 +144,6 @@ void PwmOut::resume()
         PwmOut::init();
         PwmOut::write(_duty_cycle);
         PwmOut::period_us(_period_us);
-        
     }
     core_util_critical_section_exit();
 }
