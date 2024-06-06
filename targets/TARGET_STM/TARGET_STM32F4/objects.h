5e073926604215a869d8d76010d5a2bc7ebc1c6a
Jamie Smith <smit109@usc.edu>
2024-06-05 23:41:06 -0700

    STMicro: Improve PWM resolution

--- targets/TARGET_STM/TARGET_STM32F4/objects.h
+++ targets/TARGET_STM/TARGET_STM32F4/objects.h
@@ -50,9 +50,16 @@ struct port_s {
 struct pwmout_s {
     PWMName pwm;
     PinName pin;
-    uint32_t prescaler;
     uint32_t period;
-    uint32_t pulse;
+
+    // Current compare value.  Once the PWM counter becomes >= this value,
+    // the PWM turns off.
+    uint32_t compare_value;
+    
+    // How many counts the PWM timer makes before it resets.
+    // Example: if counts = 3, the timer will count 0, 1, 2, 0, 1, 2, etc.
+    uint16_t counts;
+
     uint8_t channel;
     uint8_t inverted;
 };
