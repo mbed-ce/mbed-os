e366429ee92355e03f2d9644ed0d467550d99cc0
Jan Kamidra <odiin@windowslive.com>
2024-07-20 14:40:46 +0200

    Change source of us ticker from TIM2 to TIM3

--- targets/TARGET_STM/TARGET_STM32H5/us_ticker_data.h
+++ targets/TARGET_STM/TARGET_STM32H5/us_ticker_data.h
@@ -25,15 +25,15 @@ extern "C" {
 #include "cmsis_nvic.h"
 
 
-#define TIM_MST      TIM2
-#define TIM_MST_IRQ  TIM2_IRQn
-#define TIM_MST_RCC  __TIM2_CLK_ENABLE()
-#define TIM_MST_DBGMCU_FREEZE  __HAL_DBGMCU_FREEZE_TIM2()
+#define TIM_MST      TIM3
+#define TIM_MST_IRQ  TIM3_IRQn
+#define TIM_MST_RCC  __TIM3_CLK_ENABLE()
+#define TIM_MST_DBGMCU_FREEZE  __HAL_DBGMCU_FREEZE_TIM3()
 
-#define TIM_MST_RESET_ON   __TIM2_FORCE_RESET()
-#define TIM_MST_RESET_OFF  __TIM2_RELEASE_RESET()
+#define TIM_MST_RESET_ON   __TIM3_FORCE_RESET()
+#define TIM_MST_RESET_OFF  __TIM3_RELEASE_RESET()
 
-#define TIM_MST_BIT_WIDTH  32 // 16 or 32
+#define TIM_MST_BIT_WIDTH  16 // 16 or 32
 
 #define TIM_MST_PCLK  1 // Select the peripheral clock number (1 or 2)*/
 
