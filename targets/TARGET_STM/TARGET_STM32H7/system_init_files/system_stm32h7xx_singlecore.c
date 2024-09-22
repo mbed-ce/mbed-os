bd39481da402f5aedee0c216cf1107e698fc826d
Jamie Smith <smit109@usc.edu>
2024-09-21 18:31:21 -0700

    Fix some license issues

--- targets/TARGET_STM/TARGET_STM32H7/system_init_files/system_stm32h7xx_singlecore.c
+++ targets/TARGET_STM/TARGET_STM32H7/system_init_files/system_stm32h7xx_singlecore.c
@@ -27,9 +27,7 @@
   * Copyright (c) 2017 STMicroelectronics.
   * All rights reserved.
   *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
+  * SPDX-License-Identifier: BSD-3-Clause
   *
   ******************************************************************************
   */
