08d27de09fe3905e622786b5b977439e6dfbba29
Sam Lefebvre <sam.lefebvre@saleconix.be>
2024-12-11 17:00:21 +0100

    Adaptation of linker script to accept configured flash and ram settings

--- targets/TARGET_STM/TARGET_STM32U5/TARGET_STM32U575xG/cmsis_nvic.h
+++ targets/TARGET_STM/TARGET_STM32U5/TARGET_STM32U575xG/cmsis_nvic.h
@@ -17,23 +17,7 @@
 #ifndef MBED_CMSIS_NVIC_H
 #define MBED_CMSIS_NVIC_H
 
-#if !defined(MBED_ROM_START)
-#define MBED_ROM_START  0x8000000
-#endif
-
-#if !defined(MBED_ROM_SIZE)
-#define MBED_ROM_SIZE  0x100000 // 1MB
-#endif
-
-#if !defined(MBED_RAM_START)
-#define MBED_RAM_START  0x20000000
-#endif
-
-#if !defined(MBED_RAM_SIZE)
-#define MBED_RAM_SIZE  0xc0000 // 786K
-#endif
-
 #define NVIC_NUM_VECTORS        142
-#define NVIC_RAM_VECTOR_ADDRESS MBED_RAM_START
+#define NVIC_RAM_VECTOR_ADDRESS MBED_RAM_BANK_SRAM1_2_START
 
 #endif
