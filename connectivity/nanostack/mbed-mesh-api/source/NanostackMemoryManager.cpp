f04a0ffffcae363c8bfa5e6e08896eb7a5bc46ea
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2025-01-27 23:56:38 -0800

    Formatting

--- connectivity/nanostack/mbed-mesh-api/source/NanostackMemoryManager.cpp
+++ connectivity/nanostack/mbed-mesh-api/source/NanostackMemoryManager.cpp
@@ -158,9 +158,8 @@ NetStackMemoryManager::Lifetime NanostackMemoryManager::get_lifetime(const net_s
 
 void mbed_ns_heap_free_hook()
 {
-    auto & callback = Nanostack::get_instance().get_memory_manager().onPoolSpaceAvailCallback;
-    if(callback)
-    {
+    auto &callback = Nanostack::get_instance().get_memory_manager().onPoolSpaceAvailCallback;
+    if (callback) {
         callback();
     }
 }
