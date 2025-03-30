d4c8a1713773a3df8dec5e1c26c7b04fa8887762
Jan Kamidra <odiin@windowslive.com>
2025-03-30 10:23:09 +0200

    update clock

--- targets/TARGET_STM/TARGET_STM32U0/clock_cfg/system_clock.c
+++ targets/TARGET_STM/TARGET_STM32U0/clock_cfg/system_clock.c
@@ -22,7 +22,7 @@
   *--------------------------------------------------------------------
   * SYSCLK(MHz)           |            56
   * AHBCLK (MHz)          |            56
-  * APB1CLK (MHz)         |            56
+  * APBCLK (MHz)         |            56
   * USB capable (48 MHz)  |            YES
   *--------------------------------------------------------------------
 **/
@@ -81,82 +81,8 @@ void SetSysClock(void)
 /******************************************************************************/
 MBED_WEAK uint8_t SetSysClock_PLL_HSE(uint8_t bypass) // TODO
 {
-    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-    /** Configure the main internal regulator output voltage
-     */
-    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-
-    while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
-
-    /** Initializes the RCC Oscillators according to the specified parameters
-     * in the RCC_OscInitTypeDef structure.
-     */
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE|RCC_OSCILLATORTYPE_MSI;;
-    RCC_OscInitStruct.MSIState = RCC_MSI_ON;
-	RCC_OscInitStruct.MSICalibrationValue = RCC_MSICALIBRATION_DEFAULT;
-	RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_11;
-    if(bypass) {
-        RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
-    } else {
-        RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-    }
-
-#if (HSE_VALUE < 4000000) || (HSE_VALUE > 50000000) && !((HSE_VALUE % 2000000 != 0) || (HSE_VALUE % 5000000 != 0))
-#error HSE value must be >= 4MHz and <= 50MHz, and must be divisible by either 2 or 5!
-#endif
-if(HSE_VALUE % 2000000 == 0)
-{
-    RCC_OscInitStruct.PLL.PLLM = HSE_VALUE / 2000000; // Divide down input clock to 2MHz 
-    RCC_OscInitStruct.PLL.PLLN = 250; // Multiply up to 500MHz VCO clock
-    RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1_VCIRANGE_1;
-}
-else // Divisible by 5MHz
-{
-    RCC_OscInitStruct.PLL.PLLM = HSE_VALUE / 5000000; // Divide down input clock to 5MHz
-    RCC_OscInitStruct.PLL.PLLN = 100; // Multiply up to 500MHz VCO clock
-    RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1_VCIRANGE_2;
-}
-
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLL_SOURCE_HSE;
-
-    RCC_OscInitStruct.PLL.PLLP = 2;
-    // Most of the SPI busses are clocked off of PLL1Q, and the max usable frequency for SPI is about
-    // ~50MHz.  Plus, SPI has only limited, power-of-2 prescaler options so a high input clock really hurts
-    // its clock resolution.  So, give it a much lower input clock.
-    RCC_OscInitStruct.PLL.PLLQ = 10; // output freq = 50MHz
-    RCC_OscInitStruct.PLL.PLLR = 2;
-    RCC_OscInitStruct.PLL.PLLFRACN = 0;
-    RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1_VCORANGE_WIDE;
-    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-    {
-        return 0; // FAIL
-    }
-
-    /** Initializes the CPU, AHB and APB buses clocks
-    */
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1;
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-    {
-        return 0; // FAIL
-    }
-
-#if DEVICE_USBDEVICE
-    RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
-    PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USB;
-    PeriphClkInitStruct.UsbClockSelection = RCC_USBCLKSOURCE_MSI;
-    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK) {
-        return 0; // FAIL
-    }
-#endif /* DEVICE_USBDEVICE */
-
-    return 1; // OK
+    return 0; // FAIL
+    //return 1; // OK
 }
 #endif /* ((CLOCK_SOURCE) & USE_PLL_HSE_XTAL) || ((CLOCK_SOURCE) & USE_PLL_HSE_EXTC) */
 
