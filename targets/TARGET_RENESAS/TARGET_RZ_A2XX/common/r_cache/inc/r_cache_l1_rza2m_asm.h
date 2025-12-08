82353614ee08d0232fe179921d0126b13f16f3a4
JohnK1987 <Odiin@seznam.cz>
2025-12-08 07:50:26 +0100

    Replace ACTRL with ACTLR in r_cache_l1_rza2m_asm.h

--- targets/TARGET_RENESAS/TARGET_RZ_A2XX/common/r_cache/inc/r_cache_l1_rza2m_asm.h
+++ targets/TARGET_RENESAS/TARGET_RZ_A2XX/common/r_cache/inc/r_cache_l1_rza2m_asm.h
@@ -55,14 +55,6 @@ Includes <System Includes> , "Project Includes"
 Typedef definitions
 ******************************************************************************/
 
-#ifndef __get_ACTRL
-#define __get_ACTRL __get_ACTLR
-#endif
-
-#ifndef __set_ACTRL
-#define __set_ACTRL __set_ACTLR
-#endif
-
 /******************************************************************************
 Functions Prototypes
 ******************************************************************************/
@@ -153,12 +145,12 @@ __STATIC_FORCEINLINE void r_cache_l1_btac_inv(void)
 
 __STATIC_FORCEINLINE void r_cache_l1_prefetch_enable(void)
 {
-    __set_ACTRL(__get_ACTRL() | ACTLR_L1PE_Msk);
+    __set_ACTLR(__get_ACTLR() | ACTLR_L1PE_Msk);
 }
 
 __STATIC_FORCEINLINE void r_cache_l1_prefetch_disable(void)
 {
-    __set_ACTRL(__get_ACTRL() & (~ACTLR_L1PE_Msk));
+    __set_ACTLR(__get_ACTLR() & (~ACTLR_L1PE_Msk));
 }
 #else
 extern void r_cache_l1_cache_init(void);
