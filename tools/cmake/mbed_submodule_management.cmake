81d8fa54041baf3d8d34248e6a6a11a66ab15955
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2026-01-05 09:40:13 -0800

    Change submodule command back

--- tools/cmake/mbed_submodule_management.cmake
+++ tools/cmake/mbed_submodule_management.cmake
@@ -71,7 +71,7 @@ git submodule update --init ${SUBMODULE_PATH}")
     # determine this directory is to parse the error message.
     if(NOT "$ENV{GITHUB_RUN_ID}" STREQUAL "")
         execute_process(
-                COMMAND ${GIT_EXECUTABLE} submodule status ${SUBMODULE_PATH}
+                COMMAND ${GIT_EXECUTABLE} status
                 ERROR_VARIABLE GIT_STATUS_ERR_OUTPUT
                 RESULT_VARIABLE GIT_STATUS_RESULT_CODE
                 WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
