76bc39cbc51a322a2ad0f98488f04cdcd284a10f
Jan Kamidra <odiin@windowslive.com>
2026-03-17 23:02:48 +0100

    remove memory info from cmsis_nvic.h

--- targets/TARGET_STM/TARGET_STM32F4/TARGET_STM32F413xH/cmsis_nvic.h
+++ targets/TARGET_STM/TARGET_STM32F4/TARGET_STM32F413xH/cmsis_nvic.h
@@ -17,22 +17,6 @@
 #ifndef MBED_CMSIS_NVIC_H
 #define MBED_CMSIS_NVIC_H
 
-#if !defined(MBED_ROM_START)
-#define MBED_ROM_START  0x8000000
-#endif
-
-#if !defined(MBED_ROM_SIZE)
-#define MBED_ROM_SIZE  0x180000  // 1.5 MB
-#endif
-
-#if !defined(MBED_RAM_START)
-#define MBED_RAM_START  0x20000000
-#endif
-
-#if !defined(MBED_RAM_SIZE)
-#define MBED_RAM_SIZE  0x50000  // 320 KB
-#endif
-
 #define NVIC_NUM_VECTORS        118
 #define NVIC_RAM_VECTOR_ADDRESS MBED_RAM_START
 
