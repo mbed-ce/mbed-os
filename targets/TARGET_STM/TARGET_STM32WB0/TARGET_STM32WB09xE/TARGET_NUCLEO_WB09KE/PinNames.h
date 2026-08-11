12490490c7d321bd9bb9c11ab44247ba13b53941
Jan Kamidra <odiin@windowslive.com>
2026-08-11 21:11:43 +0200

    Reformat PinNames.h

--- targets/TARGET_STM/TARGET_STM32WB0/TARGET_STM32WB09xE/TARGET_NUCLEO_WB09KE/PinNames.h
+++ targets/TARGET_STM/TARGET_STM32WB0/TARGET_STM32WB09xE/TARGET_NUCLEO_WB09KE/PinNames.h
@@ -1,6 +1,9 @@
 /* Copyright (c) 2026 MbedCE Community Contributors (Jan Kamidra)
  * SPDX-License-Identifier: Apache-2.0
  */
+
+/* MBED TARGET LIST: NUCLEO_WB09KE */
+
 #ifndef MBED_PINNAMES_H
 #define MBED_PINNAMES_H
 
@@ -10,27 +13,43 @@
 #define GPIO_PINMAP_READY 1
 
 typedef enum {
-    PA_0 = 0x00, PA_1 = 0x01, PA_2 = 0x02, PA_3 = 0x03,
-    PA_8 = 0x08, PA_9 = 0x09, PA_10 = 0x0A, PA_11 = 0x0B,
-    PB_0 = 0x10, PB_1 = 0x11, PB_2 = 0x12, PB_3 = 0x13,
-    PB_4 = 0x14, PB_5 = 0x15, PB_6 = 0x16, PB_7 = 0x17,
-    PB_12 = 0x1C, PB_13 = 0x1D, PB_14 = 0x1E, PB_15 = 0x1F,
+    PA_0 = 0x00,
+    PA_1 = 0x01,
+    PA_2 = 0x02,
+    PA_3 = 0x03,
+    PA_8 = 0x08, 
+    PA_9 = 0x09,
+    PA_10 = 0x0A,
+    PA_11 = 0x0B,
+    PB_0 = 0x10, 
+    PB_1 = 0x11,
+    PB_2 = 0x12,
+    PB_3 = 0x13,
+    PB_4 = 0x14,
+    PB_5 = 0x15,
+    PB_6 = 0x16,
+    PB_7 = 0x17,
+    PB_12 = 0x1C,
+    PB_13 = 0x1D,
+    PB_14 = 0x1E,
+    PB_15 = 0x1F,
     NC = (int) 0xFFFFFFFF,
 
     CONSOLE_TX = PA_1,
     CONSOLE_RX = PB_0,
-
-    LED1 = PB_1,
-    LED2 = PB_4,
-    LED3 = PB_2,
-    BUTTON1 = PA_0,
-    BUTTON2 = PB_5,
-    BUTTON3 = PB_14,
 
     DEBUG_SWDIO = PA_2,
     DEBUG_SWCLK = PA_3,
     RCC_OSC32_IN = PB_13,
     RCC_OSC32_OUT = PB_12
 } PinName;
+
+#define LED1        PB_1
+#define LED2        PB_4
+#define LED3        PB_2
+
+#define BUTTON1     PA_0
+#define BUTTON2     PB_5
+#define BUTTON3     PB_14
 
 #endif
