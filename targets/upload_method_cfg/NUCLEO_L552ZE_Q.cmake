4beb757fb456c00075b8e1e1519b3e0b2f766e5f
Jan Gerrit Gers <76938266+61ca52@users.noreply.github.com>
2026-08-23 15:50:56 +0200

    STM32L: add upload methods for L475VG L476RG L496ZG L552ZE_Q based boards (#611)

---
+++ targets/upload_method_cfg/NUCLEO_L552ZE_Q.cmake
@@ -0,0 +1,57 @@
+# Mbed OS upload method configuration file for target NUCLEO_L552ZE_Q.
+# To change any of these parameters from their default values, set them in your build script between where you
+# include mbed_toolchain_setup and where you add mbed os as a subdirectory.
+
+# Notes:
+# 1. For JLINK connect directly or convert its ST-LINK into a J-Link:
+#      https://kb.segger.com/Connecting_to_STM32_Nucleo_boards
+#      https://www.segger.com/products/debug-probes/j-link/models/other-j-links/st-link-on-board/
+# 2. To use this target with PyOCD, you need to install a pack:
+#      `pyocd pack update && pyocd pack install STM32L552ZETxQ`
+# 3. For OpenOCD use Version 0.11 or newer
+# 4. For STLINK use Version 1.8.0 or newer
+
+# General config parameters
+# -------------------------------------------------------------
+set(UPLOAD_METHOD_DEFAULT MBED)
+
+# Config options for MBED
+# -------------------------------------------------------------
+
+set(MBED_UPLOAD_ENABLED TRUE)
+set(MBED_RESET_BAUDRATE 115200)
+
+# Config options for JLINK
+# -------------------------------------------------------------
+
+set(JLINK_UPLOAD_ENABLED TRUE)
+set(JLINK_CPU_NAME STM32L552ZE)
+set(JLINK_CLOCK_SPEED 4000)
+set(JLINK_UPLOAD_INTERFACE SWD)
+
+# Config options for PYOCD
+# -------------------------------------------------------------
+
+set(PYOCD_UPLOAD_ENABLED TRUE)
+set(PYOCD_TARGET_NAME stm32l552zetxq)
+set(PYOCD_CLOCK_SPEED 4000k)
+
+# Config options for OPENOCD
+# -------------------------------------------------------------
+
+set(OPENOCD_UPLOAD_ENABLED TRUE)
+set(OPENOCD_CHIP_CONFIG_COMMANDS
+    -f board/st_nucleo_l5.cfg)
+
+# Config options for STM32Cube
+# -------------------------------------------------------------
+
+set(STM32CUBE_UPLOAD_ENABLED TRUE)
+set(STM32CUBE_CONNECT_COMMAND -c port=SWD reset=HWrst)
+set(STM32CUBE_GDBSERVER_ARGS --swd)
+
+# Config options for stlink
+# -------------------------------------------------------------
+
+set(STLINK_UPLOAD_ENABLED TRUE)
+set(STLINK_ARGS --connect-under-reset)
