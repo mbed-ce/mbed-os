d0c2e72179884467314f4ed3157aa492d5fa0550
Jan Kamidra <odiin@windowslive.com>
2026-09-23 22:05:12 +0200

    Remove LSE oscillator configuration from SetSysClock for STM32WB targets

--- targets/TARGET_STM/TARGET_STM32WB/TARGET_STM32WB15xC/system_clock.c
+++ targets/TARGET_STM/TARGET_STM32WB/TARGET_STM32WB15xC/system_clock.c
@@ -73,10 +73,9 @@ void SetSysClock(void)
     Config_HSE();
 
     /* Initializes the CPU, AHB and APB busses clocks */
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_HSE | RCC_OSCILLATORTYPE_LSE;
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_HSE;
     RCC_OscInitStruct.HSEState = RCC_HSE_ON;
     RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-    RCC_OscInitStruct.LSEState = RCC_LSE_ON;
     RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
     RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
     if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
